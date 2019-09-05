d6d40ea0fe2fa88ae3fcaab5a60d59c0af9a2c3e
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-09-05 14:42:17 -0400

    Play again button added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,2 @@
+// create alphabet variable for the users to choose from
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
