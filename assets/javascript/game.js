16f0a1932778696015d5c5a88f4685a47e50a62a
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-09-08 22:29:15 -0400

    Documentation of code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,38 +8,45 @@ var currentWord = '';
 var answer = [];
 // var to hold wins initial value.
 var wins = 0;
-// Array to hold the users guess. Whatever the user clicks goes into holdWord.
-var holdWord = [];
-// variable to hold the guess.
+// Array to hold the users guess. Whatever the user clicks goes into guessedLetters.
+var guessedLetters = [];
+// variable to hold the guess charecter clicked each time.
 var userGuess = '';
 // variable to hold guesses remaining.
 var guessesRemaining = 10;
 var isWin = false;
-// When the user clicks "play" the game picks a word and populates the number of lines
-//corresponding to the number of letters in the chosen word. This is a placeholder for the random word
+
+/** When the user clicks "play" the game picks a word and populates the number of lines
+corresponding to the number of letters in the chosen word. This is a placeholder for the random word **/
+
 function pickAWord() {
+  //a random word is picked from Words array and assigned to the variable currentWord
   currentWord = words[1+ Math.floor(Math.random() * words.length)];
+  //This line puts the number of guesses on the div element called "Guesses"
   document.getElementById("guesses").innerHTML = "Guesses Remaining: " + guessesRemaining;
+
+  // This block is creating placeholder '-' for the random word upto the size of currentWord
   for (var i = 0; i < currentWord.length; i++) {
     answer[i] = "_";
     document.getElementById("blanks").innerHTML = answer;
    // console.log(answer);
-    // replacing the commas from in between the lines in the anser array by spacer .
+    // replacing the commas from in between the lines in the answer array by spacer using arrays join function.
     var remove = document.getElementById("blanks");
     remove.innerHTML = answer.join(" ");
   }
 };
 
-// Function that takes the users guess and pushes that letter to the holdWord array.
+/**  Function that takes the users guess and pushes that letter to the guessedLetters array.*/
+
 function pickALetter(letter) {
   // move letter to correct space.
-  if (!holdWord.includes(letter)) {
-    holdWord.push(letter);
-    userGuess = holdWord[holdWord.length - 1];
+  if (!guessedLetters.includes(letter)) {
+    guessedLetters.push(letter);
+    userGuess = guessedLetters[guessedLetters.length - 1];
   }
 
   // For loop that takes in the users guess and compares it to the current word's letters at each Index number.
-  // If the letter they clicked (holdWord index) is equal to the currentWord's index then the program replaces the blank spaces with that letter.
+  // If the letter they clicked (guessedLetters index) is equal to the currentWord's index then the program replaces the blank spaces with that letter.
   for (var j = 0; j < currentWord.length; j++) {
     if (userGuess === currentWord.charAt(j)) {
       answer[j] = userGuess;
@@ -98,7 +105,7 @@ function reset() {
  currentWord = '';
  answer = [];
  //wins = 0;
- holdWord = [];
+ guessedLetters = [];
  userGuess = '';
  guessesRemaining = 10;
  isWin = false;
